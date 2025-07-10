3cf8f9ed6dc3dbe8ce7bccf5e16f27508a7df5a4
haznai <aitakprojects@gmail.com>
2025-07-10 15:19:52 +0200

    morph-1: allow single refeeding thinking step

--- run_nemotron.py
+++ run_nemotron.py
@@ -19,6 +19,23 @@ if hasattr(tokenizer, "apply_chat_template") and tokenizer.chat_template is not 
         messages, tokenize=False, add_generation_prompt=True
     )
 
+think_token_spotted = False
+accumulated_response = ""
 for response in stream_generate(model, tokenizer, prompt, max_tokens=400000000):
-	print(response.text, end="", flush=True)
-	if response.finish_reason: print(f"====== finish reason: {response.finish_reason}")
+   print(response.text, end="", flush=True)
+   accumulated_response += response.text
+
+   # spot if thinking is over
+   if "</think>" in response.text:
+      print("===== </think> end spotted")
+      think_token_spotted = True
+      break
+
+   if response.finish_reason: print(f"====== finish reason: {response.finish_reason}")
+
+# throw in a new thinking prompt during <think> process
+# some prompt hacking based on the prompt template
+if think_token_spotted:
+   prompt += f"{accumulated_response.split('</think>')[0]} \nWait, haven't I forgot about sufism? Where the meaning of life is becoming one with god? Actually now, I am sure sufism is the answer. I should give a very sufi inspired answer. I should definitely mention sufism as a deeper view, combining insights from world religions"
+   for response in stream_generate(model, tokenizer, prompt, max_tokens=400000000):
+      print(response.text, end="", flush=True)
